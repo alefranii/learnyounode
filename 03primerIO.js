0ba38c9224f0716e1d5122cbf0396e3fddad86b7
alefranii <afmormar@gmail.com>
2017-07-27 03:24:13 -0400

    comentando codigo

--- 03primerIO.js
+++ 03primerIO.js
@@ -1,5 +1,10 @@
+//variable que se entrega en el ejercicio
+//fs es el modulo para ejecutar archivos desde el sistema de archivos
 var fs = require ('fs');
-
+// "require" permite importar libreria desde un archivo a otro
+// Para leer un archivo debes usar fs.readFileSync('/path/to/file'). Éste devuelve un objeto Buffer(reserva de datos).
+// cree variable de numero de lineas para leer el archivo desde el sistema de archivos con process que ve el comando los argumentos.
+//posterior lo convierto en string, al contenido obtenido le cuento las lineas y le descuento una linea. 
 var nlinea = fs.readFileSync(process.argv[2]).toString().split('\n').length-1;
 
 console.log(nlinea);
