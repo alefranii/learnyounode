5921e52a226b310526a5d41e3d396df21c6f929b
alefranii <afmormar@gmail.com>
2017-07-27 17:10:51 -0400

    hello

---
+++ 05lsFiltrado.js
@@ -0,0 +1,10 @@
+// La función fs.readdir() recibe como parámetros: una ruta(path) y un
+// callback. La firma del callback es:
+//function callback (error, lista) { /* ... */ }
+
+var fs = require('fs');
+
+//path nos dice que extencion tiene un archivo o la ruta del archivo
+var path = require('fs');
+
+//leyendo la ruta de los archivos. 
